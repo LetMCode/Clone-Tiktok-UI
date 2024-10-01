8edecd4b7fcbbf4e218c1f8f11ff5240f1582758
LetMCode <hoab2203718@student.ctu.edu.vn>
2024-10-01 17:39:55 +0700

    update- Header__css

--- src/components/Layout/DefaultLayout/index.js
+++ src/components/Layout/DefaultLayout/index.js
@@ -1,12 +1,15 @@
+import classNames from "classnames/bind";
+import styles from "./DefaultLayout.module.scss"
 import Header from "~/components/Layout/components/Header";
 import Sidebar from "./Sidebar";
 
+const cx = classNames.bind(styles)
 function DefaultLayout({children}) {
     return ( <div>
         <Header/>
-        <div className="container">
+        <div className={cx('container')}>
             <Sidebar />
-            <div className="content">
+            <div className={cx('content')}>
             {children}
             </div> 
         </div>
--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,7 +1,38 @@
+import classNames from 'classnames/bind';
+import styles from './Header.module.scss';
+import images from '~/assets/images';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+    faCircleXmark,
+    faSpinner,
+    faMagnifyingGlass,
+} from '@fortawesome/free-solid-svg-icons';
+const cx = classNames.bind(styles);
+
 function Header() {
-    return ( <h2>
-        Header
-    </h2> );
+    return (
+        <header className={cx('wrapper')}>
+            <div className={cx('inner')}>
+                <div className={cx('logo')}>
+                    <img src={images.logo} alt="Tiktok" />
+                </div>
+                <div className={cx('search')}>
+                    <input placeholder="Tìm kiếm" spellCheck={false} />
+                    <button className={cx('clear')}>
+                        <FontAwesomeIcon icon={faCircleXmark} />
+                    </button>
+                    <FontAwesomeIcon
+                        className={cx('loading')}
+                        icon={faSpinner}
+                    />
+                    <button className={cx('search-btn')}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </button>
+                </div>
+                <div className={cx('action')}></div>
+            </div>
+        </header>
+    );
 }
 
 export default Header;
